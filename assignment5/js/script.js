49936f2497569385545e77ef8f3b166adbbc646b
Ariel Armijo Q <arielarmijo@yahoo.es>
2020-08-27 14:04:28 -0400

    Remove unnecessary lines of code

--- assignment5/js/script.js
+++ assignment5/js/script.js
@@ -102,8 +102,7 @@ console.log(categories);
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories);
-console.log(chosenCategoryShortName);
-console.log(chosenCategoryShortName.short_name);
+
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
@@ -116,12 +115,10 @@ console.log(chosenCategoryShortName.short_name);
       // it into the home html snippet.
       //
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName.short_name + "'");
-console.log(homeHtmlToInsertIntoMainPage);
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
-      // ....
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
