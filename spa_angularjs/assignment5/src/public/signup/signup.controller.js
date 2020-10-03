242c0229c16455a183e5c1119cb168993cbe57ae
Ariel Armijo Q <arielarmijo@yahoo.es>
2020-10-03 00:54:56 -0300

    Fix

--- spa_angularjs/assignment5/src/public/signup/signup.controller.js
+++ spa_angularjs/assignment5/src/public/signup/signup.controller.js
@@ -1,8 +1,8 @@
 (function () {
-    "use stric";
+    "use strict";
 
-    angular.module("public")
-    .controller("SignupController", SignupController);
+    angular.module('public')
+    .controller('SignupController', SignupController);
 
     SignupController.$inject = ['MyInfoService', 'MenuService', 'ApiPath'];
     function SignupController(MyInfoService, MenuService, ApiPath) {
@@ -13,7 +13,10 @@
         ctrl.itemSearched = false;
         ctrl.basePath = ApiPath;
 
+        console.log('SignupController instantiated');
+
         ctrl.setMyinfo = function() {
+            console.log('SignupController.setMyinfo()');
             MyInfoService.setMyinfo(ctrl.userInfo);
             ctrl.saved = true;
         };
@@ -21,22 +24,25 @@
         ctrl.validateFavdish = function() {
             ctrl.validShortCode = false;
             ctrl.itemSearched = false;
+            console.log('in SignupController.validateFavdish():', ctrl.userInfo);
 
             if(typeof ctrl.userInfo.favoriteDish === 'undefined') return;
             if(ctrl.userInfo.favoriteDish.trim().length <= 0) return;
 
             MenuService.getMenuItem(ctrl.userInfo.favoriteDish).then(
                 function(response) {
+                    console.log('SignupController.validateFavdish() :)', response.data);
                     ctrl.userInfo.MenuItem = response.data;
                     ctrl.validShortCode = true;
                     ctrl.itemSearched = true;
                 },
                 function(response) {
+                    console.log('SignupController.validateFavdish() :(', response.data);
                     ctrl.itemSearched = true;
                 }
             );
         };
 
-    }
 
+    }
 })();
